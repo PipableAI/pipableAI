e5c9a0d595852e5ad3f387702745c00b2e811043
Arth Raj <csart@localhost.localdomain>
2023-07-07 13:08:53 +0530

    Google output format changed

--- search_engine/google.py
+++ search_engine/google.py
@@ -9,15 +9,14 @@ class _google_search():
   def __init__(self,_google_search=None,frame=None,
                past_query=[],current_query=None,current_input=None,
                action=[],current_action=None,
-               output=[],current_output=None,correct_output=[],programmable_search_engine_api_key="",past_results_urls=[],past_snippets=[]):
+               output=[],current_output=None,correct_output=[],programmable_search_engine_api_key="",past_results=[]):
     super().__init__()
     self.past_query=past_query
     self.current_query=current_query
     self.current_input=current_input
     self._google_search = _google_search
     self.correct_output=correct_output
-    self.past_results_urls = past_results_urls
-    self.past_snippets = past_snippets
+    self.past_results = past_results
     self.programmable_search_engine_api_key = programmable_search_engine_api_key
 
   def initialise(self,google_api_key = "",search_engine_key=""):
@@ -40,23 +39,20 @@ class _google_search():
     print("Query :- {}".format(query))
     response = gs.cse().list(q=query, cx=self.programmable_search_engine_api_key).execute()
     ite=1
-    list_of_snippets = []
-    list_of_urls = []
     #print(response)
+    output = []
     for item in response['items']:
       cleantext = clean_text(item['snippet'])
       pprint("Link {}:- {}".format(ite,item['link']))
       pprint("Result {}:- {}".format(ite,cleantext))
       ite+=1
-      list_of_urls.append(item['link'])
-      list_of_snippets.append(cleantext)
+      output.append({"url":item['link'],"headline":cleantext})
       pprint(display(HTML('<span style="color:#DB4437">Google search: Next result : </span>')))
-    self.past_results_urls.append(list_of_urls)
-    self.past_snippets.append(list_of_snippets)
+    self.past_results.append(output)
     pprint(display(HTML('<span style="color:#DB4437">Ada: Is there anything else I can do ? You can check my action catalogue to know all my skills and you can use .correct_output, .output , .querry , .input , .actions, .ada_thread methods to check our peer development thread. </span>')))
     pprint(display(HTML('<span style="color:#0F9D58">Ada: You can also ask google or open ai using .ask_google() or .ask_open_ai() methods </span>')))
     # return self
-    return list(zip(list_of_urls,list_of_snippets))
+    return output
 
   def get_latest_google_search_results(self):
     num_of_results = len(self.past_snippets)
