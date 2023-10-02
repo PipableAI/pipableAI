ce1708bd327891f5e52905f15d6ae7d640437124
avikothari84 <avikothari84@gmail.com>
2023-10-02 12:29:43 +0530

    update NOTICE and setup.py file.

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     long_description="""Pipable is a Python package designed to simplify the process of connecting to remote PostgreSQL servers and executing natural language-based data search queries. Powered by a state-of-the-art language model, Pipable translates user-friendly search queries into SQL commands, making data retrieval and analysis effortless. With a user-friendly interface and robust backend, Pipable is the ideal tool for data analysts, developers, and anyone working with complex databases.""",
     long_description_content_type="text/plain",
     author="PipableAI",
-    author_email="dev@pipbale.ai",
+    author_email="dev@pipable.ai",
     url="https://github.com/PipableAI/pipable-released",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["pandas", "psycopg2", "logging"],
+    install_requires=["pandas", "psycopg2"],
 )
